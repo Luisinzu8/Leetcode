ed0cd88951dd26cbc8c44594d5da48c8f95a1059
Luis Inzunza <luisinzu08@gmail.com>
2019-06-23 11:55:58 -0700

    syntax

--- Algorithms/twoSum.js
+++ Algorithms/twoSum.js
@@ -1,1 +1,30 @@
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//Example:
+
+//Given nums = [2, 7, 11, 15], target = 9,
+
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) { //Runtime: 64 ms
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if ((nums[i] + nums[j]) === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+//Input: [2,7,11,15], target: 9
+//Output: [0,1]
+//Expected: [0,1]
