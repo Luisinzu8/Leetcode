6925f2c6ad02cc0cab3316e684fd76bd31de1510
Luis Inzunza <luisinzu08@gmail.com>
2019-06-24 04:36:07 -0700

    add solution

--- Algorithms/validParenthese.js
+++ Algorithms/validParenthese.js
@@ -1,1 +1,30 @@
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
+// The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+var isValid = function(s) { // 100 ms runtime
+    var stack = [],
+        i;
+    s = s.split("");
+
+    for( i = 0; i < s.length; i++ ){
+        switch( s[i] ){
+            case "(":
+                stack.unshift( ")" );
+                break;
+            case "[":
+                stack.unshift( "]" );
+                break;
+            case "{":
+                stack.unshift( "}" );
+                break;
+            default:
+                if( stack.shift() !== s[i] )
+                    return false;
+                break;
+        }
+    }
+
+    if( stack.length === 0 ) return true;
+    return false;
+};
