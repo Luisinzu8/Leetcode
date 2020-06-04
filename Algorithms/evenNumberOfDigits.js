f5b41cb53cb9e574ec0975dce21c41cc2279b2e8
Luis Inzunza <luisinzu08@gmail.com>
2020-06-03 21:45:50 -0700

    add comment explaining code

--- Algorithms/evenNumberOfDigits.js
+++ Algorithms/evenNumberOfDigits.js
@@ -3,5 +3,6 @@
  * @return {number}
  */
 var findNumbers = function(nums) {
+	// use filter method to return a new array that checks if number is modulus 2
     return nums.filter(x => x.toString().length % 2 == 0).length;
 };
