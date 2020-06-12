c635fd321488d05bc7046c1b561fb38eaef626e1
Luis Inzunza <luisinzu08@gmail.com>
2020-06-11 19:42:19 -0700

    add comments explaining question

--- Algorithms/dayOfTheWeek.js
+++ Algorithms/dayOfTheWeek.js
@@ -3,7 +3,16 @@
  * @param {number} month
  * @param {number} year
  * @return {string}
+ Given a date, return the corresponding day of the week
+ for that date.
+
+The input is given as three integers representing 
+the day, month and year respectively.
+
+Return the answer as one of the following values 
+{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
  */
+
 var dayOfTheWeek = function(day, month, year) {
     let currentDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
      let inputDate = new Date (`${month}-${day}-${year}`);
