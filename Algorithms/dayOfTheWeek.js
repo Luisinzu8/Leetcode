4fc5cf1dd229ec67acf8521fb886621e93af78ba
Luis Inzunza <luisinzu08@gmail.com>
2020-06-12 21:39:16 -0700

    diff solution

--- Algorithms/dayOfTheWeek.js
+++ Algorithms/dayOfTheWeek.js
@@ -18,3 +18,9 @@ var dayOfTheWeek = function(day, month, year) {
      let inputDate = new Date (`${month}-${day}-${year}`);
     return currentDay[inputDate.getDay()];
 };
+
+var dayOfTheWeek = function(day, month, year) {
+    const date = new Date(year, month - 1, day);
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return days[date.getDay()];
+};
