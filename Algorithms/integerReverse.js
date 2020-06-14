4efd9318619db4098255f2c45f3a805204149517
Luis Inzunza <luisinzu08@gmail.com>
2020-06-14 11:11:15 -0700

    first solution

--- Algorithms/integerReverse.js
+++ Algorithms/integerReverse.js
@@ -1,9 +1,14 @@
 /**
  * @param {number} x
  * @return {number}
- Given a 32-bit signed integer, reverse digits of an integer.
+ Given a 32-bit signed integer, 
+ reverse digits of an integer.
  */
-var reverse = (x) => {
+var reverse = function(x) {
+    var reverseNum=parseInt(x.toString().split("").reverse().join(""));
     
-}
+    if(reverseNum>(Math.pow(2,31)-1)) return 0;
+    
+    return x>=0?reverseNum:0-reverseNum;
+};
     
