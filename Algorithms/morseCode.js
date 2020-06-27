25801a639d57289dc93a4bb70315256a13c1db85
Luis Inzunza <luisinzu08@gmail.com>
2020-06-27 09:44:56 -0700

    new solution

--- Algorithms/morseCode.js
+++ Algorithms/morseCode.js
@@ -1,9 +1,45 @@
-const uniqueMorseRepresentations = function(words) {
-  const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]   
-  return words.reduce((acc, word, i) => {
-    let code = '';
-    for (const letter of word) code += morse[letter.charCodeAt(0) - 97];
-    if (!acc.some(val => val === code)) acc.push(code);
-    return acc;
-  }, []).length;
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+let uniqueMorseRepresentations = (words) => {
+    const morseCodeMap = {
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--.."}
+    if(!words) return;
+    let morseArr = [];
+    for(let word of words){
+      let morseWord;
+      for(let morse of word){
+        morseWord += morseCodeMap[morse]
+      }
+      if(!morseArr.includes(morseWord)){
+        morseArr.push(morseWord);
+      }
+    }
+    return morseArr.length;
 };
