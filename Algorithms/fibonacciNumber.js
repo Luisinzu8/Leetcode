9855e21aa12b92f94719abee7d32ff451000bc22
Luis Inzunza <luisinzu08@gmail.com>
2020-07-02 22:10:59 -0700

    add description

--- Algorithms/fibonacciNumber.js
+++ Algorithms/fibonacciNumber.js
@@ -2,6 +2,13 @@
  * @param {number} N
  * @return {number}
  */
+ /*
+ The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), for N > 1.
+Given N, calculate F(N)
+*/
 let fib = (N) => {
   if (N === 0 || N === 1) return N;
   let a = 0;
