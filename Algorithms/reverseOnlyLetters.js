2906deaf5417e706e01613e6b40b0ef9cf58ddd8
Luis Inzunza <luisinzu08@gmail.com>
2020-07-03 23:51:38 -0700

    add explanation for algorithm

--- Algorithms/reverseOnlyLetters.js
+++ Algorithms/reverseOnlyLetters.js
@@ -2,21 +2,24 @@
  * @param {string} S
  * @return {string}
  */
-let reverseOnlyLetters = function(S) {
-  // if string is less than 2 letters return the string
-  if (S.length < 2) return S;
-  // create a constant variable to split the string
-  const a = S.split('');
-  //create 2 variables to iterate through constant a string
-  let i = 0;
-  let j = S.length - 1;
-  while (i < j) {
-    while (!/[a-zA-Z]/.test(S[i])) i++;
-    while (!/[a-zA-Z]/.test(S[j])) j--;
-    if (i < j) [a[i], a[j]] = [a[j], a[i]];
-    i++;
-    j--;
-  }
-  //return constant a string with .join method to combine the letters
-  return a.join('');
+let reverseOnlyLetters = (S) => {
+    // if string is less than 2 letters return string
+    if(S.length < 2) return S;
+    //use split method to split string into substrings
+    let letters = S.split('');
+    //use 2 pointer technique to iterate through letters. one pointer starts from the beginning
+    let i = 0;
+    //this pointer starts from the end
+    let j = S.length - 1;
+    while(i < j) {
+        // while iterating through letters, if its not a letter skip to the next character 
+        while (!/[a-zA-Z]/.test(S[i])) i++;
+        while (!/[a-zA-Z]/.test(S[j])) j--;
+        // while iterating and pointers have not met in the middle yet, reverse positions of the letters[i] with letters[j]
+        if (i < j) [letters[i], letters[j]] = [letters[j], letters[i]];
+        i++;
+        j--;
+    }
+    // use join method to combine our previously split string into one string
+    return letters.join('')
 };
