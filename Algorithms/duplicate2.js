293e2ab42e0b657563573b62e9f8e246abbe588c
Luis Inzunza <luisinzu08@gmail.com>
2020-07-05 12:21:58 -0700

    for loop solution

--- Algorithms/duplicate2.js
+++ Algorithms/duplicate2.js
@@ -3,6 +3,18 @@
  * @param {number} k
  * @return {boolean}
  */
+ /*
+ Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+ example:
+ Input: nums = [1,0,1,1], k = 1
+ Output: true
+ */
 var containsNearbyDuplicate = function(nums, k) {
-    
+    let map ={};
+    for(let i = 0; i < nums.length; i++) {
+    	if(map[nums[i]] >= 0 && i - map[nums[i]] <= k) return true;
+    	map[nums[i]] = i;
+    }
+    return false;
 };
