6f909218c16e73f30344b20d9b03053a67301c78
Luis Inzunza <luisinzu08@gmail.com>
2020-07-06 23:45:33 -0700

    second solution

--- Algorithms/duplicate2.js
+++ Algorithms/duplicate2.js
@@ -30,3 +30,23 @@ var containsNearbyDuplicate = function(nums, k) {
     }
     return false;
 };
+
+let containsNearbyDuplicate = (nums, k) => {
+  let map = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (map.hasOwnProperty(nums[i])) {
+            if (i - map[nums[i]] <= k) {
+                return true;
+            } 
+            else {
+                map[nums[i]] = i;
+            }
+        }
+        else {
+            map[nums[i]] = i;
+        }
+    }
+    return false;
+    
+};
