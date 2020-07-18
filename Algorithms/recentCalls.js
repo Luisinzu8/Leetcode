dbe0d1ea05f181b7827e98c13214cd0693b69a33
Luis Inzunza <luisinzu08@gmail.com>
2020-07-18 16:35:18 -0700

    add directions

--- Algorithms/recentCalls.js
+++ Algorithms/recentCalls.js
@@ -1,3 +1,12 @@
+/*
+It has only one method: ping(int t), where t represents some time in milliseconds.
+
+Return the number of pings that have been made from 3000 milliseconds ago until now.
+
+Any ping with time in [t - 3000, t] will count, including the current ping.
+
+It is guaranteed that every call to ping uses a strictly larger value of t than before.
+*/
 
 let RecentCounter = () => {
     this.queue = [];
