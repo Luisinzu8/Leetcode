e4d78e7bd0c82e3f30c521f17d8144379ae4c9ad
Luis Inzunza <luisinzu08@gmail.com>
2021-02-06 08:55:27 -0800

    add comments

--- Algorithms/searchInsertPosition.js
+++ Algorithms/searchInsertPosition.js
@@ -18,12 +18,17 @@ let nums =[1,3,5,6];
 let target= 5;
 
 var searchInsert = function(nums, target) {
+    //use for loop to iterate through the array
     for (let i = 0; i < nums.length; i++) {
+        //create currentIndex variable to keep track of our current place in the loop
         let currentIndex = nums[i];
+        //if our current index position is greater than or equal to our target number
+        //return index of the target if found
         if (currentIndex >= target) {
             return i;
         };
     };
+    //return the length if target is not found but where it would be located
     return length;
 };
 
