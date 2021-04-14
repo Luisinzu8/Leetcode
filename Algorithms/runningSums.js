f096df8d699d5bc4146f8e9e07af87a83a5656ef
Luis Inzunza <luisinzu08@gmail.com>
2021-04-14 13:14:43 -0700

    add comments to solutions, add seperate solution using seperate array

--- Algorithms/runningSums.js
+++ Algorithms/runningSums.js
@@ -2,9 +2,34 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-let runningSum = (nums) => {
-  for (let i = 1; i < nums.length; ++i) {
-    nums[i] += nums[i - 1];
+
+ let nums = [1,2,3,4]
+
+
+// let runningSum = (nums) => {
+//   //initiate loop at 1 because first number in the array will remain the same
+//   for (let i = 1; i < nums.length; ++i) {
+//     //modify nums array by reassigning our current nums index + current index - 1
+//     nums[i] += nums[i - 1];
+//   }
+//   //return modified nums array
+//   return nums;
+// };
+
+// Solution using additional array
+var runningSum = function(nums) {
+  //create new array to store running sum 
+  let result = [];
+  //assign current sum variable to 0
+  let currentSum = 0;
+  //use for loop to iterate through nums array beginning at index 0
+  for(let i = 0; i < nums.length; i++) {
+    //add current index to currentSum variable ex: (0+1 = 1), (1+2 = 3), (3+3 = 6), (6+4 = 10)
+      currentSum += nums[i];
+      //push currentSum to new array
+      result.push(currentSum)
+      //[1,3,6,10]
   }
-  return nums;
+  //return new array with runningsum of original nums array
+  return result
 };
