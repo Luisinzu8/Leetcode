8d6e4fa96ba1965f8a8352b69d2525f595a18a0a
Luis Inzunza <luisinzu08@gmail.com>
2021-05-21 12:28:33 -0700

    comment out example

--- Algorithms/containsDuplicate.js
+++ Algorithms/containsDuplicate.js
@@ -7,10 +7,10 @@
 
 Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 */
-Example 1:
+// Example 1:
 
-Input: [1,2,3,1]
-Output: true
+// Input: [1,2,3,1]
+// Output: true
 
 var containsDuplicate = function(nums) {
     return nums.length !== new Set(nums).size;
