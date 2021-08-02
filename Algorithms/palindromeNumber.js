4bea9cc67fa8d4361e05542f2864a7a3ff3938f9
Luis Inzunza <luisinzu08@gmail.com>
2021-08-02 13:10:03 -0700

    solution by converting integer to a string

--- Algorithms/palindromeNumber.js
+++ Algorithms/palindromeNumber.js
@@ -2,24 +2,41 @@
 
 // An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
-
-
 /**
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    if(x < 0) return false;
-    if(x === 0) return true;
-    return x === reverseNum(x);
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  if (x === 0) return true;
+  return x === reverseNum(x);
 };
 
 let reverseNum = (x) => {
-    
-    let reversed = 0;
-    while(x > 0) {
-        reversed = (reversed * 10) + (x % 10);
-        x = Math.floor(x / 10)
+  let reversed = 0;
+  while (x > 0) {
+    reversed = reversed * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  return reversed;
+};
+
+//integer to string solution
+
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  if (x >= 0 && x < 10) return true;
+
+  let stringNum = x.toString().split("");
+
+  let j = stringNum.length - 1;
+
+  for (let i = 0; i < stringNum.length; i++) {
+    if (stringNum[i] === stringNum[j]) {
+      j--;
+    } else {
+      return false;
     }
-    return reversed;
-}
+  }
+  return true;
+};
